535fe45b9da479b692a17c69dd65c3a25353b53f
matthew-dreemurr <hadadmat@gmail.com>
2023-05-29 15:16:21 +0200

    add button

--- index.js
+++ index.js
@@ -16,23 +16,17 @@ client.on('ready', () => {
             timestamps: {
                 start: Date.now()
             },
-            // assets: {
-            //     large_image: config.LargeImage,
-            //     large_text: config.LargeImageText,
-            //     small_image: config.SmallImage,
-            //     small_text: config.SmallImageText,
-            // },
-        //    buttons: [{
-        //            label: config.Button1,
-        //            url: config.Url1
-        //        },
-        //        {
-        //            label: config.Button2,
-        //            url: config.Url2
-        //        },
-        //         labels are the buttons that you wanna provide to your rich presence and urls are the links that leads you when someone press that button
-        //         Note the button won't work for you but don't worry it work for others
-        //     ]
+            assets: {
+                large_image: config.LargeImage,
+                large_text: config.LargeImageText,
+                small_image: config.SmallImage,
+                small_text: config.SmallImageText,
+            },
+           buttons: [{
+                   label: "Show in the cluster",
+                   url: `https://meta.intra.42.fr/clusters#${process.env.CLUSTERPOS}`
+               },
+            ]
         }
     })
 })
