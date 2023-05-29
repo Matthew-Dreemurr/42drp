ad4fcd4d7101cd1b0b72c61d34cbd477a1136fd9
matthew-dreemurr <hadadmat@gmail.com>
2023-05-29 14:44:23 +0200

    fix

--- drp.sh
+++ drp.sh
@@ -1,3 +1,3 @@
 export CLUSTERPOS=$(hostname | awk -F'.' '{print $1}')
 export CLUSTER=$(hostname | awk -F'-' '{print $1}')
-/usr/local/bin/node index.js
+/usr/local/bin/node /Users/mahadad/bin/42drp/index.js
