a0cb921572c330180a6f8270df94cae5f76b8729
matthew-dreemurr <hadadmat@gmail.com>
2023-05-31 10:33:36 +0200

    remove app and minor rework

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const rpc = require("discord-rpc");
 const client = new rpc.Client({ transport: 'ipc' });
-const config = require('./config.json');
 
-client.login({ clientId: config.ClientID }).catch(console.error);
+const config = {
+	ClientID : "526871343145287680",
+	LargeImage : "https://campus19.be/wp-content/uploads/2022/06/19logowhite-150x150.png",
+	LargeImageText : "iii",
+	Details : `Available in ${process.env.CLUSTER}`,
+	State: `${process.env.CLUSTERPOS}`
+}
+
+client.login({ clientId: "526871343145287680" }).catch(console.error);
 
 client.on('ready', () => {
-    console.log('[DEBUG] Presence now active!')
-    console.log('[WARN] Do not close this Console as it will terminate the rpc')
-    console.log('=================== Error Output ===================')
-    client.request('SET_ACTIVITY', {
-        pid: process.pid,
-        activity: {
-            details: `Available in ${process.env.CLUSTER}`,
-            state: `${process.env.CLUSTERPOS}`,
-            timestamps: {
-                start: Date.now()
-            },
-            assets: {
-                large_image: config.LargeImage,
-                large_text: config.LargeImageText,
-                small_image: config.SmallImage,
-                small_text: config.SmallImageText,
-            },
-           buttons: [{
-                   label: "Show in the cluster",
-                   url: `https://meta.intra.42.fr/clusters#${process.env.CLUSTERPOS}`
-               },
-            ]
-        }
-    })
+	console.log('[DEBUG] Presence now active!')
+	client.request('SET_ACTIVITY', {
+		pid: process.pid,
+		activity: {
+			details: config.Details,
+			state: config.State,
+			timestamps: {
+				start: Date.now()
+			},
+			assets: {
+				large_image: config.LargeImage,
+				large_text: config.LargeImageText,
+				// small_image: config.SmallImage,
+				// small_text: config.SmallImageText,
+			},
+		buttons: [{
+				label: "Show in the cluster",
+				url: `https://meta.intra.42.fr/clusters#${process.env.CLUSTERPOS}`
+			},
+			]
+		}
+	})
 })
